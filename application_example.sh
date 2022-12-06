ee21959bef7ef5a2fdc2f1ef9ea7c641f35a275c
johanna-malle <78730637+johanna-malle@users.noreply.github.com>
2022-12-06 13:25:23 +0100

    Update application_example.sh

--- application_example.sh
+++ application_example.sh
@@ -1,14 +1,19 @@
+w5e5_regrid
+
 #!/bin/sh
 
-source activate W5E5_regrid_env
+source activate /home/malle/miniconda3/envs/W5E5_regrid_env
+cd /home/malle/slfhome/Postdoc3/python-scripts/pythonProject/adhoris_regrid/
 
 # do bias adjustment for all variables in one program call
-python -u w5e5_regrid/downscale_wind.py \
---input_path_w5e5 /home/storage/malle/W5E5_global \
---input_path_wind_atlas /home/storage/malle/CLM5_alps/gwa3_250_wind-speed_10m_alps.tif \
---input_target_grid /home/storage/malle/CLM5_alps/chelsa_input/chelsa-w5e5v1.0_obsclim_mask_30arcsec_global.nc \
---output_loc /home/malle/Desktop/temp_download/test \
+python -u w5e5_regrid/downscale_all.py \
+--input_path_w5e5 /storage/malle/W5E5_global \
+--input_path_wind_atlas /storage/malle/CLM5_alps/gwa3_250_wind-speed_10m_alps.tif \
+--input_target_grid /storage/malle/CLM5_alps/chelsa_input/chelsa-w5e5v1.0_obsclim_mask_30arcsec_global.nc \
+--output_loc /home/malle/test_run \
 --extent_target 42.25,48.75,2.25,17.25 \
---var_interest sfcWind \
+--var_interest ps,sfcWind,rh \
 --year_start 1979 \
 --year_end 1982 \
+--input_path_orog /storage/malle/CLM5_alps/chelsa_input/chelsa-w5e5v1.0_obsclim_orog_30arcsec_global.nc \
+--input_path_chelsa /storage/brunp/Data/CHELSA_V.2.1/RH/Monthly \
