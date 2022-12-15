35e3ce62ddd2e2e8a91b8a2e99424ababc855248
johanna-malle <78730637+johanna-malle@users.noreply.github.com>
2022-12-15 11:16:55 +0100

    include longwave options

--- application_example.sh
+++ application_example.sh
@@ -12,8 +12,10 @@ python -u w5e5_regrid/downscale_all.py \
 --input_target_grid /storage/malle/CLM5_alps/chelsa_input/chelsa-w5e5v1.0_obsclim_mask_30arcsec_global.nc \
 --output_loc /home/malle/test_run \
 --extent_target 42.25,48.75,2.25,17.25 \
---var_interest ps,sfcWind,rh \
---year_start 1979 \
+--var_interest ps,sfcWind,rh, rlds \
+--year_start 1981 \
 --year_end 1982 \
 --input_path_orog /storage/malle/CLM5_alps/chelsa_input/chelsa-w5e5v1.0_obsclim_orog_30arcsec_global.nc \
 --input_path_chelsa /storage/brunp/Data/CHELSA_V.2.1/RH/Monthly \
+--input_path_tas_fine /home/malle/Desktop/temp_download/test/tas \
+--input_path_rh_fine /home/malle/Desktop/temp_download/test/rh \
